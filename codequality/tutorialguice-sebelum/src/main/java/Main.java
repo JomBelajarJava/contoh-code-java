3112881ae6d89b050e8dd1dd3d18e0039558d65d
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2018-11-26 14:35:20 +0800

    Update Main.java

--- codequality/tutorialguice-sebelum/src/main/java/Main.java
+++ codequality/tutorialguice-sebelum/src/main/java/Main.java
@@ -11,5 +11,6 @@ public class Main {
                 new GreetingRepositoryImpl());
 
         System.out.println(greetingService.getGreeting("Kassim"));
+        System.out.println(greetingService.getGreeting("Johnny"));
     }
 }
